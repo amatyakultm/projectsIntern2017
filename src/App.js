068770c5fbb65cbaf7838316a23acad7a0165c9f
benzero <5706021630097@fitm.kmutnb.ac.th>
2017-06-27 16:49:48 +0700

    i chang branch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,113 @@
 import React, { Component } from 'react';
+<<<<<<< Updated upstream
 import logo from './logo.svg';
+=======
+import axios from 'axios'
+import _ from 'lodash'
+>>>>>>> Stashed changes
+import './App.css';
+import Modal from './Modal';
+import { Button } from 'react-bootstrap';
+import dataUsr from './dataUser.json';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userPopUp: '',
+      projectPopUp: '',
+      failedPopUp: false,
+
+
+
+    };
+  }
+  clickShowPopup = (event) => {
+    event.preventDefault();
+
+    this.getUsers()
+
+  }
+  getUsers = () => {
+
+    this.setState({
+      userPopUp: dataUsr
+
+    })
+    // console.log(this.state.users)
+
+
+    /*axios.get('http://localhost:3000/home/ubuntu-benz/Desktop/projectsIntern2017/src/dataUser.json')
+      .then(response => {
+        this.setState({
+          users: response.data.users
+        })
+      })
+      .catch(err => {
+        this.setState({
+          failed: true
+        })
+      })*/
+  }
+
+
+  render() {
+
+    if (this.state.failedPopUp) return <h3>Get User Failed.</h3>
+
+    return (
+      <div className="App">
+        <input onClick={this.clickShowPopup} type="button" className="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal" value="ada" />
+        <Modal userPopUp={this.state.userPopUp} />
+
+
+
+      </div>
+
+    );
+  }
+}
+
+export default App;
+/*
+import React, { Component } from 'react';
+import axios from 'axios'
+import _ from 'lodash'
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      failed: false
+    }
+  }
+
+  getUsers = () => {
+    axios.get('https://8efa9ba5.ngrok.io/users')
+      .then(response => {
+        this.setState({
+          users: response.data.users
+        })
+      })
+      .catch(err => {
+        this.setState({
+          failed: true
+        })
+      })
+  }
+  componentDidMount() {
+    this.getUsers()
+  }
   render() {
+    var userData = _.map(this.state.users, (user, index) => {
+      return <p key={`user-${index}`}>{user.id}</p>
+    })
+    if (this.state.failed) return <h3>Get User Failed.</h3>
     return (
       <div className="App">
+<<<<<<< Updated upstream
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
@@ -13,9 +115,19 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+=======
+        <h2>Welcome to React</h2>
+        <hr />
+        <h4>list User Data</h4>
+        <br />
+        <div>
+          {!this.state.users ? 'Loading..' : userData}
+        </div>
+>>>>>>> Stashed changes
       </div>
     );
   }
 }
 
 export default App;
+*/
---
+++ src/Modal.js
@@ -0,0 +1,141 @@
+import React, { Component } from 'react';
+import _ from 'lodash';
+import Search from './component/Search';
+import Table from '/home/ubuntu-benz/Desktop/projectsIntern2017/src/component/Table.js';
+
+import { Button, InputGroup, FormGroup, form, FormControl, BootstrapTable, TableHeaderColumn } from 'react-bootstrap';
+
+//import { Table } from 'react-bootstrap';
+
+class Modal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userToTable: ''
+
+
+    };
+  }
+
+
+
+  render() {
+
+    this.setState({ userToTable: this.props.userPopUp })
+    // const formInstance = <div className="row">
+    //   <div className="col-lg-10">
+    //     <div className="input-group">
+
+    //       <input type="text" className="form-control" placeholder="Search for..." />
+    //       <span className="input-group-btn">
+    //         <button className="btn btn-default" type="button">search</button>
+    //       </span>
+    //     </div>
+    //   </div>
+    // </div>
+    /*const table = <table className="table table-bordered">
+      <thead>
+        <tr className="active">
+          <td>Date</td>
+          <td>Tasks</td>
+          <td>Work hrs</td>
+        </tr>
+      </thead>
+      <tr><td>1</td><td>a</td><td>3</td></tr>
+    </table>*/
+
+
+
+    //     var userData = _.map(this.props.userPopUp, (user, index) => {
+    //      return <p key={`user-${index}`}>{user.id}</p>
+    // })
+
+    // var user = _.map(this.props.userPopUp.Benz, (user2, index) => { return <div>{user2.date}</div> })
+    /*<BootstrapTable>
+
+    </BootstrapTable>*/
+    return (
+
+
+      <div className="container" >
+        <div className="modal fade" id="myModal" role="dialog" >
+          <div className="modal-dialog modal-lg" >
+
+
+            <div className="modal-content" >
+
+
+
+
+              <div className="modal-header">
+                <button type="button" className="close" data-dismiss="modal">&times;</button>
+              </div>
+
+
+              <Search />
+
+
+
+
+              {/*!this.props.userPopUp ? 'Loading' : user*/}
+              {!this.props.userPopUp ? 'Loading..' :
+                <div>
+                  <Table vauleColumnHead={['Date', 'Task', 'Workour']} userPopUp={this.props.userPopUp} /></div>}
+
+
+
+
+
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+
+    );
+  }
+}
+
+export default Modal;
+
+
+
+
+
+/*const Modal = (props) => {
+  
+
+  return (
+    <div className="App">
+
+            <div className="container">
+
+
+
+
+
+              <div className="modal fade" id="myModal" role="dialog">
+                <div className="modal-dialog">
+
+
+                  <div className="modal-content">
+                    {props.name}
+                    <div className="modal-header">
+                      <button type="button" className="close" data-dismiss="modal">&times;</button>
+
+                    </div>
+
+
+                  </div>
+
+                </div>
+              </div>
+
+            </div>
+          </div>
+          );
+
+}
+
+export default Modal;*/
