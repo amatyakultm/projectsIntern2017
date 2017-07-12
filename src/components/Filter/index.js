5e9c677ac9b7d0dff3e2f28c11f5dfe351c8aa97
amatyakultm <maykira.amatyakul@gmail.com>
2017-07-12 16:02:34 +0700

    commit commit2

---
+++ src/components/Filter/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import style from './style.css'
+
+class Filter extends Component {
+
+    render() {
+        return (
+            <div>
+                filter box
+            </div>
+        )
+    }
+}
+export default Filter;
