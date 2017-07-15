ca90f5f88a1c8647738ca71131d6d9fe8b100866
tammopinkid <benjarat.saeang@gmail.com>
2017-07-15 15:52:48 +0700

    update

---
+++ src/components/PresentAbsent/Filter.js
@@ -0,0 +1,150 @@
+import React, { Component } from 'react';
+import './stylefilter.css';
+
+class Filter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      absent: '',
+      normal: '',
+      overwork: '',
+      underwork: ''
+    };
+    this.onChangeAbsent = this.onChangeAbsent.bind(this);
+    this.onChangeNormal = this.onChangeNormal.bind(this);
+    this.onChangeOverwork = this.onChangeOverwork.bind(this);
+    this.onChangeUnderwork = this.onChangeUnderwork.bind(this);
+  }
+
+  onChangeAbsent(e) {
+    this.setState({
+      absent: !this.state.absent
+    });
+    this.state.absent == false
+      ? this.setState({ absent: e.target.value })
+      : this.setState({ absent: '' });
+  }
+
+  onChangeNormal(e) {
+    this.setState({
+      normal: !this.state.normal
+    });
+    this.state.normal == false
+      ? this.setState({ normal: e.target.value })
+      : this.setState({ normal: '' });
+  }
+
+  onChangeOverwork(e) {
+    this.setState({
+      overwork: !this.state.overwork
+    });
+    this.state.overwork == false
+      ? this.setState({ overwork: e.target.value })
+      : this.setState({ overwork: '' });
+  }
+
+  onChangeUnderwork(e) {
+    this.setState({
+      underwork: !this.state.underwork
+    });
+    this.state.underwork == false
+      ? this.setState({ underwork: e.target.value })
+      : this.setState({ underwork: '' });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="dropdown" style={{ marginTop: '20px' }}>
+          <button
+            className="btn btn-danger dropdown-toggle"
+            type="button"
+            id="dropdownMenuButton"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            Filter
+          </button>
+          <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <input
+              id="checkbox-1"
+              className="checkbox-custom input"
+              name="checkbox-1"
+              type="checkbox"
+              style={{
+                marginLeft: '20px',
+                width: '19px',
+                height: '19px',
+                backgroundColor: 'white'
+              }}
+              type="checkbox"
+              value="level=Normal"
+              checked={!this.state.absent}
+              onChange={this.onChangeAbsent}
+            />
+            <label for="checkbox-1" className="checkbox-custom-label">
+              Absent
+            </label>{' '}
+            <br />
+            <input
+              id="checkbox-1"
+              className="checkbox-custom input"
+              name="checkbox-1"
+              type="checkbox"
+              style={{
+                marginLeft: '20px',
+                width: '19px',
+                height: '19px',
+                backgroundColor: 'white'
+              }}
+              type="checkbox"
+              value="level=Normal"
+              checked={!this.state.normal}
+              onChange={this.onChangeNormal}
+            />
+            <label for="checkbox-1" className="checkbox-custom-label">
+              Normal
+            </label>{' '}
+            <br />
+            <input
+              id="checkbox-1"
+              className="checkbox-custom input"
+              name="checkbox-1"
+              type="checkbox"
+              style={{ marginLeft: '20px', width: '19px', height: '19px' }}
+              type="checkbox"
+              value="level=Overwork"
+              checked={!this.state.overwork}
+              onChange={this.onChangeOverwork}
+            />
+            <label for="checkbox-1" className="checkbox-custom-label">
+              Overwork
+            </label>{' '}
+            <br />
+            <input
+              id="checkbox-1"
+              className="checkbox-custom input"
+              name="checkbox-1"
+              type="checkbox"
+              style={{ marginLeft: '20px', width: '19px', height: '19px' }}
+              type="checkbox"
+              value="level=Underwork"
+              checked={!this.state.underwork}
+              onChange={this.onChangeUnderwork}
+            />
+            <label for="checkbox-1" className="checkbox-custom-label">
+              Underwork
+            </label>{' '}
+            <br />
+            {/*<div>
+                            <input id="checkbox-1" className="checkbox-custom input" name="checkbox-1" type="checkbox" />
+                            <label for="checkbox-1" className="checkbox-custom-label">First Choice</label>
+                        </div>*/}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default Filter;
--- src/pages/projectDetail.js
+++ src/pages/projectDetail.js
@@ -3,10 +3,24 @@ import { defaults, Pie } from 'react-chartjs-2';
 import axios from 'axios';
 import _ from 'lodash';
 import ReactLoading from 'react-loading';
+import Loading from '../components/Loading';
 import Style from '../styles/Style.css';
 import TableData from '../components/ProjectOverview/TableData';
 
 defaults.global.legend.display = false;
+const positionList = {
+  'Project Management Officer': { id: 'PMO', color: '#EE1F79' },
+  'Frontend Developer': { id: 'F.DEV', color: '#9E65AB' },
+  'Backend Developer': { id: 'B.DEV', color: '#7360AC' },
+  'Quality Assurance Engineer': { id: 'QA', color: '#0052A6' },
+  'Business Analyst': { id: 'BA', color: '#00A7BC' },
+  Designer: { id: 'DSN', color: '#04A54A' },
+  'Mobile Developer': { id: 'M.DEV', color: '#8FC630' },
+  'HR Director': { id: 'HR', color: '#FFF200' },
+  Technology: { id: 'TECH', color: '#FFB700' },
+  'Application Support': { id: 'SUP', color: '#F98B20' },
+  'Co-Founder': { id: 'CO', color: '#F46A1C' }
+};
 class ProjectDetail extends Component {
   constructor(props) {
     super(props);
@@ -15,57 +29,82 @@ class ProjectDetail extends Component {
       sum_hours: 0,
       isOpen: false
     };
-    console.log(props.params.projectid);
+  }
+
+  async componentDidMount() {
+    try {
+      const { data } = await axios.get(
+        `http://52.77.234.30/api/sumprojectposition/${this.props.params
+          .projectid}`
+      );
+      this.setState({
+        projectdetail: data.sumprojects,
+        from: data.start,
+        to: data.end
+      });
+      this.getProjectData();
+      _.map(this.state.projectdetail, (item, index) => {
+        const _position = item[Object.keys(item)[0]];
+        const listLabelOption = _.map(_position, ({ position }) => position);
+        console.log('po', listLabelOption);
+
+        const option = {
+          datasets: [
+            {
+              data: _.map(_position, ({ total_hour }) => total_hour),
+              backgroundColor: _.map(listLabelOption, label =>
+                _.get(positionList, [label, 'color'])
+              )
+            }
+          ],
+          labels: listLabelOption,
+          maintainAspectRatio: false,
+          responsive: true
+        };
+        console.log(option);
+        this.setState({ option: option });
+      });
+      // console.log(this.state.projectdetail[0]);
+    } catch (err) {
+      this.setState({ failed: true });
+    }
   }
 
   getProjectData() {
     axios
       .get(
-        `http://52.77.234.30/projects/${this.props
-          .project_id}/detail?start=${this.props.start}&end=${this.props.end}`
+        `http://52.77.234.30/projects/${this.props.params
+          .projectid}/detail?start=${this.state.from}&end=${this.state.to}`
       )
       .then(response => {
         this.setState({
-          projectdetail: response.data
+          projectData: response.data
         });
-        console.log(this.state.projectdetail.projectData);
+        console.log(this.state.projectData);
       })
       .catch(err => {
         this.setState({
           failed: true
         });
-        //console.log(err)
       });
   }
 
-  componentDidMount() {
-    this.getProjectData();
-  }
-
   render() {
-    const loadingData = () => {
-      return (
-        <div className="col align-self-center loading">
-          <img src="./assets/img/loading.svg" alt="" width="50" />
-        </div>
-      );
-    };
-
     const createData = () => {
       return (
         <div className="row mt-5">
           <div className="col-lg-3 col-md-4 col-sm-6 col-12">
             <div className="card-box">
               <h6 className="text-mute text-center">
-                {parseInt(this.props.sum_hours / 1000 / 60 / 60)} Hrs
+                {parseInt(this.state.projectdetail[0].sum / 1000 / 60 / 60)} Hrs
               </h6>
-              <Pie data={this.props.option} width={420} height={420} />
+              <Pie data={this.state.option} width={420} height={420} />
               <div className="col-md-12 text-center mt-3">
                 {/*<button key={`btn-details-${index}`} onClick={() => this.handleClickDetail(Object.keys(item)[0])} className='btn btn-danger btn-sm btn-details'>Details</button>*/}
                 {/*<Link to={`/project/${Object.keys(item)[0]}`}>
                   <button key={`btn-details-${index}`} className='btn btn-danger btn-sm btn-details'>Details</button>
                 </Link>*/}
-                {this.state.projectdetail.project_name}
+                {this.state.projectdetail[0].projectname}
               </div>
             </div>
             <div className="row">
@@ -116,12 +155,11 @@ class ProjectDetail extends Component {
               <i className="zmdi zmdi-globe" /> Project Details{' '}
               {!this.state.projectdetail
                 ? ''
-                : `From ${this.state.projectdetail.from} To ${this.state
-                    .projectdetail.to}`}
+                : `From ${this.state.from} To ${this.state.to}`}
             </h4>
           </div>
         </div>
-        {!this.state.projectdetail ? loadingData() : createData()}
+        {!this.state.projectdetail ? <Loading /> : createData()}
       </div>
     );
   }
